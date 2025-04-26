031840fe6e6a06305a90780803c89e9a67543a60
tufail ahmad <ta4990047@gmail.com>
2025-04-27 00:20:04 +0500

    Routes created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,31 @@
-export default function App() {
+import React from 'react'
+import Login from './Pages/Auth/Login';
+import SingUp from './Pages/Auth/SignUp';
+import Home from './Pages/Dashboard/Home';
+import Income from './Pages/Dashboard/Income'
+import Expense from './Pages/Dashboard/Expense'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router";
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      <button className="btn btn-secondary">aaaaa</button>
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Root />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SingUp />} />
+        <Route path="/dashboard" element={<Home />} />
+        <Route path="/income" element={<Income />} />
+        <Route path="/expense" element={<Expense />} />
+        
+      </Routes>
+    </BrowserRouter>
   )
+}
+export default App
+
+const Root = () => {
+  const isAuthenticated=!!localStorage.getItem("tokken")
+  return isAuthenticated ? 
+  (<Navigate to="/dashboard"/>)
+  :
+  (<Navigate to='/login'/>)
 }
