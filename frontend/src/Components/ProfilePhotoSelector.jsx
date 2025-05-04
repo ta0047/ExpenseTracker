3fe543119031f6df3852134a6cebcf1ec4ceb759
tufail ahmad <ta4990047@gmail.com>
2025-05-04 17:14:28 +0500

    add pic in signup

--- frontend/src/Components/ProfilePhotoSelector.jsx
+++ frontend/src/Components/ProfilePhotoSelector.jsx
@@ -1,26 +1,53 @@
-import React, { useRef, useState } from 'react'
-const ProfilePhotoSelector = ({image,setImage}) => {
-    const inputRef = useRef(null)
-    const [previewUrl, setpreviewUrl] = useState(null)
+import React, { useRef, useState, useEffect } from 'react'
+import { FaUserCircle } from "react-icons/fa";
+import { MdDelete, MdOutlineFileUpload } from "react-icons/md";
+const ProfilePhotoSelector = ({ image, setImage }) => {
+  useEffect(() => {
+    setpreviewUrl(null);
+  }, []);
+  const inputRef = useRef(null)
+  const [previewUrl, setpreviewUrl] = useState(null)
 
-    const handleImageChange=(event)=>{
-        const file=event.target.file(0);
-        if(file){setImage(file)}
-        const preview=URL.createObjectURL(file);
-        setpreviewUrl(preview)
-    }
-    const handleRemoveImage=()=>{
-        setImage(null)
-        setpreviewUrl(null)
-    }
-    const onChooseFile=()=> {
-        inputRef.current.click()
-        
-    }
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    if (file) { setImage(file) }
+    const preview = URL.createObjectURL(file);
+    setpreviewUrl(preview)
+  }
+  const handleRemoveImage = () => {
+    setImage(null)
+    setpreviewUrl(null)
+    if (inputRef.current) inputRef.current.value = "";
+  }
+  const onChooseFile = () => {
+    inputRef.current.click()
+  }
   return (
     <>
-    <div>{image}</div>
-    <div>{setImage}</div>
+      <input
+        type="file"
+        accept='image/*'
+        ref={inputRef}
+        onChange={handleImageChange}
+        className='hidden'
+      />
+      <div className="relative w-32 h-32">
+      {previewUrl ? (
+    <img
+      src={previewUrl}
+      alt="Profile Pic"
+      className="w-full h-full object-cover rounded-full"
+    />
+  ) : (
+    <FaUserCircle className="w-full h-full text-gray-400" />
+  )}
+        <button
+          className="btn btn-xs bg-indigo-500 rounded-full text-slate-100 absolute bottom-0 right-0"
+          onClick={previewUrl ? handleRemoveImage : onChooseFile}
+        >
+          {previewUrl ? <MdDelete size={20} /> : <MdOutlineFileUpload size={20} />}
+        </button>
+      </div>
     </>
   )
 }
--- frontend/src/Pages/Auth/SignUp.jsx
+++ frontend/src/Pages/Auth/SignUp.jsx
@@ -10,7 +10,7 @@ const SignUp = () => {
   const [lastname, setlastname] = useState("")
   const [email, setemail] = useState("")
   const [password, setpassword] = useState("")
-  const [image, setImage] = useState("image")
+  const [image, setImage] = useState(null)
   const [profilePic, setprofilePic] = useState("profile")
   const { register, handleSubmit, formState: { errors } } = useForm();
   const navigate = useNavigate();
@@ -24,10 +24,13 @@ const SignUp = () => {
           <h1 className='text-3xl text-slate-600 text-center'>SIGNUP</h1>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
 
-            <div className="bg-teal-200 w-32 m-auto h-32 rounded-full md:col-span-2 ">
+            <div className="w-32 m-auto h-32 rounded-full md:col-span-2">
               <ProfilePhotoSelector 
               image={profilePic} 
-              setImage={setprofilePic}/>
+              setImage={setprofilePic}
+             
+              />
+              
             </div>
             <div className="">
               <label htmlFor="firstname" className="block mb-1">Enter First Name</label>
